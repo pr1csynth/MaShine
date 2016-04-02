a3b918814abf9c21f834abf12b74943ca897284c
procsynth <procsynth@users.noreply.github.com>
2016-04-02 23:45:19 +0200

    Added text logo to menu

--- src/mashine/ui/boxes/Menu.java
+++ src/mashine/ui/boxes/Menu.java
@@ -11,11 +11,13 @@ import mashine.*;
 import mashine.ui.*;
 import mashine.ui.elements.*;
 import java.util.ArrayList;
+import processing.core.PImage;
 
 public class Menu extends Drawable{
 	
 	private ArrayList<Element> bigButtons;
 	private ArrayList<Element> elements;
+	private PImage mashineImage;
 
 	
 	public Menu(){
@@ -36,57 +38,57 @@ public class Menu extends Drawable{
 		Do open = new Do(){public void x(){MaShine.inputs.getAction("mashine.open").x();}};
 		Do restore = new Do(){public void x(){MaShine.inputs.getAction("mashine.restore").x();}};
 
-		bigButtons.add(new TextButton(this, 15, 0, 120, 32, "PATCH", 
+		bigButtons.add(new TextButton(this, 115, 0, 120, 32, "PATCH", 
 			Colors.WHITE, 60,
 			Colors.MATERIAL.CYAN._600,
 			Colors.MATERIAL.CYAN._700,
 			switchToScene));
-		bigButtons.add(new TextButton(this, 140, 0, 120, 32, "ANIMATE", 
+		bigButtons.add(new TextButton(this, 240, 0, 120, 32, "ANIMATE", 
 			Colors.WHITE, 60,
 			Colors.MATERIAL.DEEP_PURPLE._600,
 			Colors.MATERIAL.DEEP_PURPLE._700,
 			switchToAnimate));
-		bigButtons.add(new TextButton(this, 265, 0, 120, 32, "LIVE", 
+		bigButtons.add(new TextButton(this, 365, 0, 120, 32, "LIVE", 
 			Colors.WHITE, 60,
 			Colors.MATERIAL.RED._600,
 			Colors.MATERIAL.RED._700,
 			switchToLive));
-		bigButtons.add(new TextButton(this, 390,  0, 70, 15, "event in", 
+		bigButtons.add(new TextButton(this, 502,  0, 70, 15, "event in", 
 			Colors.WHITE, 10,
 			Colors.MATERIAL.TEAL._600,
 			Colors.MATERIAL.TEAL._800,
 			openEventViewer));
-		bigButtons.add(new TextButton(this, 390, 17, 70, 15, "data  in", 
+		bigButtons.add(new TextButton(this, 502, 17, 70, 15, "data  in", 
 			Colors.WHITE, 10,
 			Colors.MATERIAL.TEAL._600,
 			Colors.MATERIAL.TEAL._800,
 			openDataViewer));
-		bigButtons.add(new TextButton(this, 465, 0, 70, 15, "links", 
+		bigButtons.add(new TextButton(this, 575, 0, 70, 15, "links", 
 			Colors.WHITE, 10,
 			Colors.MATERIAL.TEAL._600,
 			Colors.MATERIAL.TEAL._800,
 			openLinker));
 
-		bigButtons.add(new TextButton(this, 600, 0, 70, 15, "save", 
+		bigButtons.add(new TextButton(this, 650, 0, 70, 15, "save", 
 			Colors.WHITE, 10,Colors.MATERIAL.YELLOW._600,Colors.MATERIAL.YELLOW._800,
 			save));
-		bigButtons.add(new TextButton(this, 600, 17, 70, 15, "save as", 
+		bigButtons.add(new TextButton(this, 650, 17, 70, 15, "save as", 
 			Colors.WHITE, 10,Colors.MATERIAL.YELLOW._600,Colors.MATERIAL.YELLOW._800,
 			saveAs));
 
-		bigButtons.add(new TextButton(this, 675, 0, 70, 15, "open", 
+		bigButtons.add(new TextButton(this, 722, 0, 70, 15, "open", 
 			Colors.WHITE, 10,Colors.MATERIAL.YELLOW._600,Colors.MATERIAL.YELLOW._800,
 			open));
-		bigButtons.add(new TextButton(this, 675, 17, 70, 15, "restore", 
+		bigButtons.add(new TextButton(this, 722, 17, 70, 15, "restore", 
 			Colors.WHITE, 10,Colors.MATERIAL.YELLOW._600,Colors.MATERIAL.YELLOW._800,
 			restore));
+			mashineImage = MaShine.m.loadImage("mashine.png");
 	}
 
 	public void drawContent(){
 		canvas.noStroke();
-		FlatColor.fill(canvas, Colors.MATERIAL.BLUE_GREY._900);
-		canvas.rect(0, 0, width, height);
-		//canvas.rect(MaShine.mouseX, MaShine.mouseY, 20, 20);
+		FlatColor.background(canvas, Colors.MATERIAL.BLUE_GREY._900);
+		canvas.image(mashineImage, 5, 8);
 
 		for(Element bb : bigButtons){
 			bb.draw();
