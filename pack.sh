f9022222f2b38af4446aa71c9056ccaae538f3e7
procsynth <procsynth@users.noreply.github.com>
2016-04-03 02:17:44 +0200

    Added pngs to pack script

--- pack.sh
+++ pack.sh
@@ -7,6 +7,7 @@ mkdir MaShine releases
 cp dist/mashine.jar MaShine
 cp -R lib/ MaShine
 cp *.vlw MaShine
+cp *.png MaShine
 cp README.md MaShine
 tar czvf releases/MaShine_`date +%Y-%m-%d_%H-%M-%S`.tar.gz MaShine
 rm -rf MaShine
