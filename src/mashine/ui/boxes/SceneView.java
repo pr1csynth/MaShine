173c4c7abf1f265754b7bd3e3c628c49bb8eb810
procsynth <procsynth@users.noreply.github.com>
2016-04-09 20:33:33 +0200

    Basic drag selection

--- src/mashine/ui/boxes/SceneView.java
+++ src/mashine/ui/boxes/SceneView.java
@@ -18,14 +18,20 @@ import java.util.Map;
 import java.util.Iterator;
 import java.util.ArrayList;
 
+import processing.core.PConstants;
+
 public class SceneView extends Drawable {
 
 	LinkedHashMap<Device,DeviceElement> deviceElements;
 	ArrayList<Device> selectedDevices;
 	Frame displayFrame;
 
+	boolean dragging = false;
+	int dragStartX = 0;
+	int dragStartY = 0;
+
 	public SceneView(){
-		super(10, 50, MaShine.m.displayWidth - 10, MaShine.m.displayHeight - 50);
+		super(0, 40, MaShine.m.displayWidth, MaShine.m.displayHeight - 40);
 		deviceElements = new LinkedHashMap<Device, DeviceElement>();
 		selectedDevices = new ArrayList<Device>();
 		displayFrame = new Frame();
@@ -39,6 +45,8 @@ public class SceneView extends Drawable {
 
 		ArrayList<Device> devices = MaShine.scene.getDevices();
 		HashMap<String,EditableFeature> frameFeatures = frame.getFeatures();
+
+		boolean selectionMade = false;
 
 		for(Device d : devices){
 			// add new DeviceElement if not already present
@@ -54,21 +62,74 @@ public class SceneView extends Drawable {
 			if(MaShine.inputs.getState("keyboard.16.hold") && de.isClicked()){
 				if(selected){
 					selectedDevices.remove(d);
-					selected = false;
+					selected = false;	
 				}else{
 					selectedDevices.add(d);
-					selected = true;				
+					selected = true;
 				}
+				selectionMade = true;		
 			}else if(de.isClicked()){
 				selectedDevices.clear();
 				selected = true;
+				selectionMade = true;		
 				selectedDevices.add(d);
 			}
 
 			// draw it
 			de.setSelected(selected);
 			de.setFrameFeatures(frameFeatures);
-			de.draw();
+			de.draw();			
+		}
+
+		if(hasFocus() && !selectionMade && MaShine.inputs.getState("mouse.left.press") && !dragging){
+			selectedDevices.clear();
+		}
+
+		if(hasFocus() && !selectionMade  && MaShine.inputs.getState("mouse.left.drag") && !dragging){
+			dragging = true;
+			dragStartX = mouseX();
+			dragStartY = mouseY();
+		}
+
+		if(!MaShine.inputs.getState("mouse.left.drag") && dragging && hasFocus()){
+			dragging = false;
+
+			if(!MaShine.inputs.getState("keyboard.16.hold")){
+				selectedDevices.clear();
+			}
+
+			for(Device d : devices){
+				int dx = d.getX();
+				int dy = d.getY();
+				int dw = d.getWidth();
+				int dh = d.getHeight();
+
+				if(
+					Math.min(mouseX(), dragStartX) < dx && dx < Math.max(mouseX(), dragStartX) &&
+					Math.min(mouseY(), dragStartY) < dy && dy < Math.max(mouseY(), dragStartY) ||
+					Math.min(mouseX(), dragStartX) < (dx + dw) && (dx + dw) < Math.max(mouseX(), dragStartX) &&
+					Math.min(mouseY(), dragStartY) < (dy + dh) && (dy + dh) < Math.max(mouseY(), dragStartY) ||
+					Math.min(mouseX(), dragStartX) < dx && dx < Math.max(mouseX(), dragStartX) &&
+					Math.min(mouseY(), dragStartY) < (dy + dh) && (dy + dh) < Math.max(mouseY(), dragStartY) ||
+					Math.min(mouseX(), dragStartX) < (dx + dw) && (dx + dw) < Math.max(mouseX(), dragStartX) &&
+					Math.min(mouseY(), dragStartY) < dy && dy < Math.max(mouseY(), dragStartY)
+				){
+					//d.setSelected(true);
+					selectedDevices.add(d);
+				}else{
+					if(!MaShine.inputs.getState("keyboard.16.hold")){
+						//d.setSelected(false);
+					}
+				}
+			}
+		}
+
+		if(dragging){
+			canvas.rectMode(PConstants.CORNERS);
+			FlatColor.fill(canvas, Colors.MATERIAL.GREEN.A200.withAlpha(20));
+			FlatColor.stroke(canvas, Colors.MATERIAL.CYAN.A200);
+			canvas.rect(dragStartX, dragStartY, mouseX(), mouseY());
+			canvas.rectMode(PConstants.CORNER);
 		}
 
 		// remove DeviceElement if Device not here anymore
