ae7295035d6a844102f879812d4b946288676d07
procsynth <procsynth@users.noreply.github.com>
2016-04-12 10:26:46 +0200

    Fixed wrong displayed color for RGB feature

--- src/mashine/ui/elements/DeviceElement.java
+++ src/mashine/ui/elements/DeviceElement.java
@@ -73,7 +73,7 @@ public class DeviceElement extends Element{
 					// special case color, coordinates, ...
 
 				if(feature instanceof ColorFeature){
-					FlatColor.fill(P.canvas, ((ColorFeature)feature).getColor().withAlphaAsWhite());
+					FlatColor.fill(P.canvas, ((ColorFeature)feature).getColor());
 					P.canvas.noStroke();
 				}else if(feature instanceof SingleField){
 					for(String f : featureFields.keySet()){
